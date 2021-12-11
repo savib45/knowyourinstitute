cb5b5a001504fa5218d552bcc5be2bce2f992ec6
Savib Pandey <savibpandey@Savibs-MacBook-Air.local>
2021-12-11 11:08:05 +0545

    models and superuser

--- projecthackathon/knowyourinstitution/admin.py
+++ projecthackathon/knowyourinstitution/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+from .models import Review
+from .models import College
+
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(College)
--- projecthackathon/knowyourinstitution/models.py
+++ projecthackathon/knowyourinstitution/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=30)
+    contactNumber = models.CharField(max_length=15)
+    userCollegeName = models.CharField(max_length=30)
+    photos = models.ImageField(upload_to='images/')
+
+    USERNAME_FIELD = 'full_name'
+
+    def __str__(self):
+        return self.user_id
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=40)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=3000, blank=True)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.user.user_id
+
+
+class College(models.Model):
+    college_id = models.AutoField(primary_key=True)
+    college_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    website_url = models.CharField(max_length=40)
+    college_reviews = models.ForeignKey(Review, on_delete=models.CASCADE)
+    college_rating = models.IntegerField
+    college_description = models.TextField(max_length=150)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    program = models.CharField(max_length=10)
+    college_photos = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.college_id
